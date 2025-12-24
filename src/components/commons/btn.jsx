716553bd37f7d4bbfdad27d80905c94af14bc73d
Sobhan ghasemi <mohammadghasemi3033@gmail.com>
2025-12-24 21:53:38 +0330

    feat : create BTN components

---
+++ src/components/commons/btn.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Button({solid=true, children }) {
+    if (solid){
+        return (
+            <button className="px-8 py-3 bg-gradient-to-r from-green-600 to-emerald-500 hover:from-emerald-500 hover:to-green-600 rounded-xl font-bold text-lg shadow-lg shadow-emerald-900/30 transition-all duration-300 hover:shadow-emerald-900/50">
+                {children}
+            </button>
+
+        )
+    } else {
+        return (
+            <button className="px-8 py-3 border border-emerald-500/50 hover:bg-emerald-900/30 rounded-xl font-bold text-lg backdrop-blur-sm transition-all duration-300">
+                {children}
+            </button>
+        )
+    }
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { motion } from "motion/react"
+import Button from "../components/commons/btn.jsx";
 
 export default function Home(){
     return (
@@ -39,12 +40,8 @@ export default function Home(){
                             transition={{ delay: 0.7, type: "spring" }}
                             className="flex flex-col sm:flex-row gap-4 justify-center"
                         >
-                            <button className="px-8 py-3 bg-gradient-to-r from-green-600 to-emerald-500 hover:from-emerald-500 hover:to-green-600 rounded-xl font-bold text-lg shadow-lg shadow-emerald-900/30 transition-all duration-300 hover:shadow-emerald-900/50">
-                                شروع یادگیری
-                            </button>
-                            <button className="px-8 py-3 border border-emerald-500/50 hover:bg-emerald-900/30 rounded-xl font-bold text-lg backdrop-blur-sm transition-all duration-300">
-                                مشاهده دوره‌ها
-                            </button>
+                          <Button >شروع یادگیری</Button>
+                          <Button solid={false}>مشاهده دوره ها</Button>
                         </motion.div>
                     </motion.div>
                 </div>
